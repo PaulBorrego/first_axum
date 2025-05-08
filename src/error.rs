32187d39d7ef854c25987ea20bdc006ff6da72cf
Paul Borrego (Personal) <borregopaulj@gmail.com>
2025-05-07 22:04:23 -0500

    working at this point

---
+++ src/error.rs
@@ -0,0 +1,18 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+
+pub type Result<T> = core::result::Result<T,Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    NotFound,
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        println!("->> error = {self:?}");
+
+        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,57 @@
+#![allow(unused)] // for beginning only
+
+pub use self::error::{Error, Result};
+
+// backend
 use axum::{
-    routing::get,
     Router,
+    extract::{Path, Query},
+    response::{Html, IntoResponse},
+    routing::get,
 };
+
+mod error;
+mod web;
+
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().merge(routes_hello());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on http://{}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct SchedulerRequest {
+    task: Option<String>,
+}
+
+// /scheuler?task=example
+async fn handler_scheduler(Query(params): Query<SchedulerRequest>) -> impl IntoResponse {
+    println!("->> scheduler_handler - {:?}", params);
+
+    let task = params.task.as_deref().unwrap_or("No task provided");
+    Html(format!(
+        "<h1>Scheduler</h1><h2>{task}</h2><p>This is the scheduler page.</p>"
+    ))
+}
+
+// /scheduler/Paul
+async fn handler_scheduler_path(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> scheduler_handler - {:?}", name);
+
+    Html(format!(
+        "<h1>Scheduler</h1><h2>{name}</h2><p>This is the scheduler page.</p>"
+    ))
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/scheduler", get(handler_scheduler))
+        .route("/scheduler/{name}", get(handler_scheduler_path))
 }
